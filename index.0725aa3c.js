044c870817207150170ba7651f7084000a3a97c9
Yuliia-Kyivska <107933607+Yuliia-Kyivska@users.noreply.github.com>
2022-08-27 13:58:01 +0000

    Deploying to gh-pages from @ Yuliia-Kyivska/project-team-10@f52127bf38a67cf8367f3b8147630cf007e5d3e9 🚀

---
+++ index.0725aa3c.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{navigation:{nextEl:".swiper__btn-next",prevEl:".swiper__btn-prev"}});
+//# sourceMappingURL=index.0725aa3c.js.map
---
+++ index.ce4edc8e.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{navigation:{nextEl:".swiper__btn-next",prevEl:".swiper__btn-prev"}});
+//# sourceMappingURL=index.ce4edc8e.js.map
