fd5c19725b87f1bca66670ca4f48d452d1d45140
Yuliia-Kyivska <107933607+Yuliia-Kyivska@users.noreply.github.com>
2022-08-29 15:16:37 +0000

    Deploying to gh-pages from @ Yuliia-Kyivska/project-team-10@ca29c349ad6852f3df4c3b5e11d1bf3ffadd79bd 🚀

--- index.1a89d54f.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[data-menu-open]"),n=document.querySelector("[data-menu-close]"),t=document.querySelector("[data-menu]");function r(){t.classList.toggle("is-open")}e.addEventListener("click",r),n.addEventListener("click",r)}(),new Swiper(".swiper",{navigation:{nextEl:".swiper__btn-next",prevEl:".swiper__btn-prev"}});
-//# sourceMappingURL=index.1a89d54f.js.map
